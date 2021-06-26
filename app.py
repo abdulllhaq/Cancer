95258325e91d817a4099b036d5dd723f163c4544
braxtonova <85130590+braxtonova@users.noreply.github.com>
2021-06-27 00:14:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ st.write(df.describe())
 #train data. Fun!
 x = df.drop(['Outcome'], axis = 1)
 y = df.iloc[:, -1]
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.6, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 0)
 
 
 #User reports
